daac8dce2d59c9edcdb9bf662f7aae66f3047914
Connor Holmstrom <connor.holmstrom@gmail.com>
2025-07-03 14:05:18 -0700

    fix message and handle undefined case

--- src/features/teams/Teams.tsx
+++ src/features/teams/Teams.tsx
@@ -1,37 +1,42 @@
-import { useQuery } from "@tanstack/react-query"
-import { getActiveTeams } from "../../api/teams"
-import { TeamResponse } from "./team-types"
-import { Link } from "@tanstack/react-router"
+import { useQuery } from "@tanstack/react-query";
+import { getActiveTeams } from "../../api/teams";
+import { TeamResponse } from "./team-types";
+import { Link } from "@tanstack/react-router";
 
+export default function Teams() {
+  const {
+    isLoading: isLoadingTeams,
+    data: activeTeams,
+    error: error,
+  } = useQuery<TeamResponse[]>({
+    queryKey: ["teams"],
+    queryFn: () => getActiveTeams(),
+  });
 
-export default function Teams() { 
+  if (error) {
+    return <p>Error Getting Team Data</p>;
+  }
 
-    const { isLoading: isLoadingTeams, data: activeTeams, error: error, isSuccess }= useQuery<TeamResponse[]>({
-        queryKey: ['teams'], 
-        queryFn: () => getActiveTeams(), 
-    })
+  if (isLoadingTeams) {
+    return <p>Loading active NHL Teams....</p>;
+  }
 
-    if(!isSuccess || error) { 
-        return <p>Error Getting Team Data</p>
-    }
+  if (!activeTeams) {
+    return <p>No active teams found</p>;
+  }
 
-    if(isLoadingTeams) { 
-        return <p>Loading active NHL Teams....</p>
-    }
-        
-    return (
+  return (
     <>
-        <h1>Active NHL teams</h1>
-        <div className="teams-list">
-            {activeTeams
-                .map(team => { 
-                    return (
-                        <div key={team.id}>
-                            <Link to={`${team.triCode}/current`}>{team.fullName}</Link>
-                        </div>
-                    )
-                })}
-        </div>
+      <h1>Active NHL teams</h1>
+      <div className="teams-list">
+        {activeTeams.map((team) => {
+          return (
+            <div key={team.id}>
+              <Link to={`${team.triCode}/current`}>{team.fullName}</Link>
+            </div>
+          );
+        })}
+      </div>
     </>
-    )
+  );
 }
